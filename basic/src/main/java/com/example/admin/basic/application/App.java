3286c594207a34cf69399f57d1319d1ac93043e4
xingxingstar <15712883024@163.com>
2017-11-23 18:25:12 +0800

    新增basic及conn的module

---
+++ basic/src/main/java/com/example/admin/basic/application/App.java
@@ -0,0 +1,14 @@
+package com.example.admin.basic.application;
+
+import android.app.Application;
+
+/**
+ * @author Xinxin Shi
+ */
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
